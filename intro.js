29ce39f6bf56f217a968361d741e7cf7a2ce34fd
Israr Ahmad Tech <israrahmadtech@gmail.com>
2025-02-08 17:40:02 +0500

    Uploading new js repo

---
+++ intro.js
@@ -0,0 +1,15 @@
+// THEORY
+
+// Javascript: Programming language used to communicate with machine.
+
+// Interpreter - line by line execution
+// Compiler: Converts(translates) Javscript(high level language) code into machine language at run time
+
+// Single Thread : Run on one(single) thread of the processor - one core have two threads(one core can do two tasks at a time because it has two threads)
+
+// Synchronous: Second task will wait untill the fist task is done(2nd task cannot be done before first)
+
+// JIT: Just in time compilation(Interpreting and compilation woks separatly)
+
+
+// Javascript is dynamic typed language means you can change data type of variable at run time
---
+++ variable.js
@@ -0,0 +1,35 @@
+
+var age; // Declaration
+age = 20; // Initialization
+
+age = 30; // Re-initialization
+
+let description = "His father's name is Israr" // Using qoute inside string
+
+
+// name
+// age
+// roll no
+// 2 numbers 
+// percentage
+var name = prompt("What is your name")
+var rollNo = +prompt("What is your Roll No")
+var age = +prompt("What is your age")
+var numOne = +prompt("Enter first number between 0 and 10")
+var numTwo = +prompt("Enter second number between 0 and 10")
+var obtained = numOne + numTwo;
+var total = 20;
+var percentage = obtained / total * 100
+console.log("Name: ", name)
+console.log("Roll No: ", rollNo)
+console.log("Age: ", age)
+console.log("Obtained: ", obtained)
+console.log("Total: ", total)
+console.log("Percentage: ", percentage + '%')
+
+// User name
+// Mobile number
+// Email 
+
+// 5 subjects marks
+// Obtained marks
